f17e414ccfc3ce1a5b903b6a31f7b55e7c81cf3e
Saif <saifjamsheer1998@gmail.com>
2021-02-14 16:22:13 +0300

    Made changes

--- lyrics-generator/c_generator.py
+++ lyrics-generator/c_generator.py
@@ -4,72 +4,138 @@ import sys
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, LSTM
 from keras.optimizers import Adam
+from keras.callbacks import LambdaCallback
 
-#### Character-Level Text Generator ###
+"""""""""""""""""""""""""""""""""
+CHARACTER-LEVEL LYRICS GENERATOR
+"""""""""""""""""""""""""""""""""
 
+def load_lyrics(path):
+    """
+    Load lyrics from input path
+    """
+    with io.open(LYRICS_PATH, 'r', encoding='utf8') as f: lyrics = f.read()
+    return lyrics
+
+def create_sequences(text, length, step):
+    """
+    Create inputs and labels for training
+    """
+    sequences = []
+    next_chars = []
+    for i in range(0, len(text) - length, step):
+        sequences.append(text[i: i + length])
+        next_chars.append(text[i + length])
+
+    return sequences, next_chars
+
+def get_index_mappings(chars):
+    """
+    Create mappings from character to index and index to character
+    """
+    return {c: i for i, c in enumerate(chars)}, {i: c for i, c in enumerate(chars)}
+
+def vectorize_lyrics(chars, char_ix, sequences, next_chars, length):
+    """
+    Convert characters and sequences into numerical representations
+    """
+    X = np.zeros((len(sequences), SEQUENCE_LENGTH, len(chars)), dtype=np.bool)
+    y = np.zeros((len(sequences), len(chars)), dtype=np.bool)
+    for i, sentence in enumerate(sequences):
+        for t, char in enumerate(sentence):
+            X[i, t, char_ix[char]] = 1
+        y[i, char_ix[next_chars[i]]] = 1
+    return X, y
+
+def build(length, chars, n_layers=1, dropout=0.2):
+    """
+    Build the network structure and compile the model
+    """
+    model = Sequential()
+    for _ in range(n_layers):
+        model.add(LSTM(64, input_shape=(SEQUENCE_LENGTH, len(chars))))
+    model.add(Dropout(dropout))
+    model.add(Dense(len(chars)))
+    model.add(Activation('softmax'))
+    optimizer=Adam(lr=0.01)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy')
+    return model
+
+def sample(preds, temperature=1.0):
+    """
+    Samples a character index based through probabilistic means
+    """
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def on_epoch_end(epoch, logs):
+    """
+    Callback after each epoch to write the generated lyrics to the output file
+    """
+    
+    output_file.write('Generating lyrics after epoch: {}\n'.format(epoch))
+
+    sentence = seed
+    output_file.write('Generating with seed: {}\n\n'.format(sentence))
+    generated = ''
+
+    for _ in range(500):
+        x = np.zeros((1, SEQUENCE_LENGTH, len(chars)))
+        for t, char in enumerate(sentence):
+            x[0, t, char_ix[char]] = 1.0
+
+        preds = model.predict(x, verbose=0)[0]
+        next_index = sample(preds, DIVERSITY)
+
+        next_char = ix_char[next_index]
+
+        generated += next_char
+        sentence = sentence[1:] + next_char
+
+    output_file.write('Generated lyrics:\n')
+    output_file.write(generated)
+    output_file.write('\n')
+    output_file.write('-'*50 + '\n')
+
+# Defining global variables
 SEQUENCE_LENGTH = 21
 SEQUENCE_STEP = 3
 LYRICS_PATH = 'datasets/inputs.txt'
+OUTPUT_PATH = 'datasets/output.txt'
 BATCH_SIZE = 128
-EPOCHS = 5000
+EPOCHS = 2
 DIVERSITY = 1.0
 
-# Extract unique characters from lyrics
-with io.open(LYRICS_PATH, 'r', encoding='utf8') as f: text = f.read()
+# Load lyrics and extract unique characters
+text = load_lyrics(LYRICS_PATH)
 chars = sorted(list(set(text)))
 
-# Sequences are inputs into the network and next_chars will be used as labels for training
-sequences = []
-next_chars = []
-for i in range(0, len(text) - SEQUENCE_LENGTH, SEQUENCE_STEP):
-    sequences.append(text[i: i + SEQUENCE_LENGTH])
-    next_chars.append(text[i + SEQUENCE_LENGTH])
+# Create inputs (sequences) and labels (next_chars) for model training
+sequences, next_chars = create_sequences(text, SEQUENCE_LENGTH, SEQUENCE_STEP)
+char_ix, ix_char = get_index_mappings(chars)
 
-char_ix, ix_char = dict((c, i) for i, c in enumerate(chars)), dict((i, c) for i, c in enumerate(chars))
+# Convert text into numerical representations
+X, y = vectorize_lyrics(chars, char_ix, sequences, next_chars, SEQUENCE_LENGTH)
 
-# Vectorize the characters and strings
-X = np.zeros((len(sequences), SEQUENCE_LENGTH, len(chars)), dtype=np.bool)
-y = np.zeros((len(sequences), len(chars)), dtype=np.bool)
-for i, sentence in enumerate(sequences):
-    for t, char in enumerate(sentence):
-        X[i, t, char_ix[char]] = 1
-    y[i, char_ix[next_chars[i]]] = 1
+# Build the LSTM model
+model = build(SEQUENCE_LENGTH, chars, n_layers=1, dropout=0.0)
 
-model = Sequential()
-model.add(LSTM(64, input_shape=(SEQUENCE_LENGTH, len(chars))))
-model.add(Dense(len(chars)))
-model.add(Activation('softmax'))
+# Open file to output generated text
+output_file = open(OUTPUT_PATH, 'w')
 
-optimizer = Adam(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+# Define callbacks for model training
+write_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+callbacks_list = [write_callback]
 
-model.fit(X, y, batch_size=BATCH_SIZE, epochs=EPOCHS)
+# Define seed for text generation
+seed = "Today was a fairytale"
 
-generated = ''
-    
-sentence = "Today was a fairytale"
+# Model training and text generation
+model.fit(X, y, batch_size=BATCH_SIZE, epochs=EPOCHS, callbacks=callbacks_list)
 
-generated += sentence
-
-print('Generating with seed: "' + sentence + '"')
-
-for i in range(500):
-    x = np.zeros((1, SEQUENCE_LENGTH, len(chars)))
-    for t, char in enumerate(sentence):
-        x[0, t, char_ix[char]] = 1.
-
-    predictions = model.predict(x, verbose=0)[0]
-
-    preds = np.asarray(predictions).astype('float64')
-    preds = np.log(preds) / 1.0
-    exp_preds = np.exp(preds)
-    preds = exp_preds / np.sum(exp_preds)
-    probs = np.random.multinomial(1, preds, 1)
-    next_index = np.argmax(probs)
-
-    next_char = ix_char[next_index]
-
-    generated += next_char
-    sentence = sentence[1:] + next_char
-
-print(generated)
+# Close output file
+output_file.close()
--- lyrics-generator/w_generator.py
+++ lyrics-generator/w_generator.py
@@ -1,10 +1,106 @@
 import io
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, LSTM
+from keras.optimizers import Adam
 
-#### Word-Level Text Generator ###
+#### WORD-LEVEL LYRICS GENERATOR ###
 
 LYRICS_PATH = 'datasets/inputs.txt'
 BATCH_SIZE = 128
-EPOCHS = 5000
+EPOCHS = 1
 DIVERSITY = 1.0
+MIN_FREQ = 10
+SEQUENCE_LENGTH = 5
+SEQUENCE_STEP = 1
 
 with io.open(LYRICS_PATH, 'r', encoding='utf8') as f: text = f.read()
+
+text = text.replace('\n', ' \n ')
+text = text.replace('?', '')
+text = text.replace('!', '')
+text = text.replace('(', '')
+text = text.replace(')', '')
+text = text.replace('.', '')
+corpus = [w for w in text.split(' ') if w.strip() != '' or w == '\n'
+        and (w[0] not in ["(", "["] and w[-1] not in [")", "]"])]
+
+while "" in corpus:
+    corpus.remove("")
+
+word_freq = {}
+for word in corpus:
+    word_freq[word] = word_freq.get(word, 0) + 1
+
+ignored_words = set()
+for k, v in word_freq.items():
+    if word_freq[k] < MIN_FREQ:
+        ignored_words.add(k)
+
+vocab = set(corpus)
+# vocab = sorted(set(vocab) - ignored_words)
+
+word_ix, ix_word = dict((c, i) for i, c in enumerate(vocab)), dict((i, c) for i, c in enumerate(vocab))
+
+sequences = []
+next_words = []
+
+for i in range(0, len(corpus) - SEQUENCE_LENGTH, SEQUENCE_STEP):
+        sequences.append(corpus[i: i + SEQUENCE_LENGTH])
+        next_words.append(corpus[i + SEQUENCE_LENGTH])
+
+# Vectorize the words
+X = np.zeros((len(sequences), SEQUENCE_LENGTH, len(vocab)), dtype=np.bool)
+y = np.zeros((len(sequences), len(vocab)), dtype=np.bool)
+for i, sentence in enumerate(sequences):
+    for t, word in enumerate(sentence):
+        X[i, t, word_ix[word]] = 1
+    y[i, word_ix[next_words[i]]] = 1
+
+# split_count = int(0.8 * len(sequences))
+# sentences_test = sequences[split_count:]
+# next_words_test = next_words[split_count:]
+# sentences = sequences[:split_count]
+# next_words = next_words[:split_count]
+
+# print(sentences)
+model = Sequential()
+model.add(LSTM(64, input_shape=(SEQUENCE_LENGTH, len(vocab))))
+model.add(Dense(len(vocab)))
+model.add(Activation('softmax'))
+
+optimizer = Adam(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+# model.fit(X, y, batch_size=BATCH_SIZE, epochs=EPOCHS)
+
+generated = []
+seed_index = int(np.random.randint(len(sequences)))
+seed = sequences[seed_index]
+sentence = seed
+
+[generated.append(word) for word in sentence]
+print(generated)
+
+for i in range(50):
+    x = np.zeros((1, SEQUENCE_LENGTH, len(vocab)))
+    print(sentence)
+    for t, word in enumerate(sentence):
+        print(x[0, t, word_ix[word]])
+        x[0, t, word_ix[word]] = 1.
+
+    predictions = model.predict(x, verbose=0)[0]
+
+    preds = np.asarray(predictions).astype('float64')
+    preds = np.log(preds) / DIVERSITY
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probs = np.random.multinomial(1, preds, 1)
+    next_index = np.argmax(probs)
+
+    next_word = ix_word[next_index]
+
+    generated.append(next_word)
+    sentence = sentence[1:].append(next_word)
+
+print(generated)
